c44e5332b726dee89929a68ae5d6812ffc901f81
MichelMokbel <michelmokbel@hotmail.com>
2023-11-15 20:35:47 +0200

    Project: FINAL Counting and Radix Sort

--- .vscode/Project/test_Project.cpp
+++ .vscode/Project/test_Project.cpp
@@ -3,56 +3,16 @@
 
 using namespace std;
 
+class Sorting
+{
+public:
+    void radixSort(int *arr, int n);
 
-
-// void countingSort(int *arr, int n)
-// {
-//     int *A = new int[n];
-//     for (int i = 0; i < n; i++)
-//     {
-//         A[i] = arr[i];
-//     }
-//     printArray(A, n, "Original Array");
-
-//     int maxVal = A[0];
-//     for (int i = 1; i < n; i++)
-//     {
-//         if (A[i] > maxVal)
-//         {
-//             maxVal = A[i];
-//         }
-//     }
-
-//     int *B = new int[n];
-//     int *C = new int[maxVal + 1];
-
-//     for (int i = 0; i <= maxVal; i++)
-//     {
-//         C[i] = 0;
-//     }
-//     printArray(C, maxVal + 1, "Initialized Count Array");
-//     for (int j = 0; j < n; j++)
-//     {
-//         C[A[j]] = C[A[j]] + 1;
-//     }
-//     printArray(C, maxVal + 1, "Populated Count Array");
-//     for (int i = 1; i <= maxVal; i++)
-//     {
-//         C[i] = C[i] + C[i - 1];
-//     }
-//     printArray(C, maxVal + 1, "Modified Count Array");
-//     for (int j = n - 1; j >= 0; j--)
-//     {
-//         B[C[A[j]] - 1] = A[j];
-//         C[A[j]] = C[A[j]] - 1;
-//     }
-//     printArray(B, n, "Sorted Array in Counting Sort");
-
-//     for (int i = 0; i < n; i++)
-//     {
-//         arr[i] = B[i];
-//     }
-// }
+private:
+    int getMax(int *arr, int n);
+    void radixCountingSort(int *arr, int n, int exp);
+    void printArray(int arr[], int n, string msg);
+};
 
 int getMax(int *arr, int n)
 {
@@ -75,11 +35,50 @@ void printArray(int arr[], int n, string msg)
     cout << endl;
 }
 
+void countingSort(int *arr, int n)
+{
+    int *A = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        A[i] = arr[i];
+    }
+    printArray(A, n, "Original Array");
 
+    int maxVal = getMax(arr, n);
+
+    int *B = new int[n];
+    int *C = new int[maxVal + 1];
+
+    for (int i = 0; i <= maxVal; i++)
+    {
+        C[i] = 0;
+    }
+    printArray(C, maxVal + 1, "Initialized Count Array");
+    for (int j = 0; j < n; j++)
+    {
+        C[A[j]] = C[A[j]] + 1;
+    }
+    printArray(C, maxVal + 1, "Populated Count Array");
+    for (int i = 1; i <= maxVal; i++)
+    {
+        C[i] = C[i] + C[i - 1];
+    }
+    printArray(C, maxVal + 1, "Modified Count Array");
+    for (int j = n - 1; j >= 0; j--)
+    {
+        B[C[A[j]] - 1] = A[j];
+        C[A[j]] = C[A[j]] - 1;
+    }
+    printArray(B, n, "Sorted Array in Counting Sort");
+
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = B[i];
+    }
+}
 
 void radixCountingSort(int *arr, int n, int exp)
 {
-    
     cout << endl;
     cout << "Sorted array for exp = " << exp << endl;
     cout << endl;
@@ -127,10 +126,6 @@ void radixCountingSort(int *arr, int n, int exp)
     {
         arr[i] = B[i];
     }
-
-
-
-    
     printArray(arr, n, "Final Sorted Array");
 }
 
@@ -147,19 +142,41 @@ void radixSort(int *arr, int n)
 
 int main()
 {
-    int arr[] = {
-        170,
-        45,
-        75,
-        90,
-        802,
-        24,
-        2,
-        66};
-    int n = sizeof(arr) / sizeof(arr[0]);
+    int arr1[] = {170, 45, 75, 90, 802, 24, 2, 66};
+    int n1 = sizeof(arr1) / sizeof(arr1[0]);
+    radixSort(arr1, n1);
 
-    radixSort(arr, n);
+    int arr2[] = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+    int n2 = sizeof(arr2) / sizeof(arr2[0]);
+    radixSort(arr2, n2);
 
+    int arr3[] = {1, 3, 5, 7, 9, 2, 4, 6, 8, 0};
+    int n3 = sizeof(arr3) / sizeof(arr3[0]);
+    radixSort(arr3, n3);
+
+    int arr4[] = {1235, 0, 1, 555, 1899, 42};
+    int n4 = sizeof(arr4) / sizeof(arr4[0]);
+    radixSort(arr4, n4);
+
+    cout << "All test cases passed!" << endl;
 
     return 0;
 }
+
+// int main()
+// {
+//     int arr[] = {
+//         170,
+//         45,
+//         75,
+//         90,
+//         802,
+//         24,
+//         2,
+//         66};
+//     int n = sizeof(arr) / sizeof(arr[0]);
+
+//     radixSort(arr, n);
+
+//     return 0;
+// }
